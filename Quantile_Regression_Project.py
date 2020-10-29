c94b46df7a488332712ede0eda44cd44c52affa2
ybouaffad <ynbouaffad@gmail.com>
2020-10-29 14:48:13 +0100

    COMPARISON WITH RIDGE

--- Quantile_Regression_Project.py
+++ Quantile_Regression_Project.py
@@ -1,5 +1,5 @@
 """
-@authors: Yani Bouaffad, Ryma Lakehal and Loïc Sauton
+@authors: Yani Bouaffad, Ryma Lakehal and LoÃ¯c Sauton
 In short: Quantile regression using CVXCP package.
 """
 
@@ -18,6 +18,7 @@ import matplotlib.pyplot as plt
 import statsmodels.formula.api as smf
 import time
 import seaborn as sns
+from sklearn.linear_model import Ridge
 
 ################################
 # Download datasets
@@ -100,17 +101,33 @@ objective = cvx.Minimize(error)
 problem = cvx.Problem(objective)
 
 
+### Solve quantile regression for different values of  ğ¼
+# ğ¼={0.01,0.1,â¦,0.9,0.99}
+
+
 ## Solve quantile regression for 𝛼 = 0.5
 fits = np.zeros(len(X))
 
 start_time= time.time()  
+
 problem.solve(solver="ECOS")
+
+for k,a in enumerate(alpha):
+    alpha.value = a
+    problem.solve(solver="ECOS")
+
     
 for i in range(len(X)):
     fits[i] = (X[i].T*b ).value
 
+
 interval = time.time() - start_time
 print("Median prediction obtained with cvxpy package : ",np.unique(fits))
+
+
+interval= time.time() - start_time
+print(np.unique(fits, axis=1))
+print("the execution time with the implementation obtained with package cvxpy : ",interval)
 
 ################################
 # Implementation with statsmodels package
@@ -129,3 +146,63 @@ print("Median prediction obtained with package statsmodels : ",prediction)
 #################################
 print("the execution time with the implementation obtained with cvxpy package : ",interval)
 print("the execution time with the implementation obtained with package statsmodels : ",(end - start))
+
+print(prediction)
+
+
+################################
+# COMPARISON WITH RIDGE
+#################################
+
+###  Evolution of the Ridge estimator graphically
+
+def iteration_ridge_alpha(logalpha):
+    np.random.seed(0)
+    current_palette = sns.color_palette()
+    fig = sns.catplot(y="valeur_originale", x="nom_com", data=df,
+                      jitter='0.05', legend_out=False, order=list(ville),
+                      height=8)
+
+    plt.axhline(y=np.mean(valeur_o3), xmin=0, xmax=len(ville), ls='--',
+                linewidth=3, color='grey')
+    plt.text(len(ville)-0.3, np.mean(valeur_o3), '$\\bar y$', color='grey',
+             verticalalignment='bottom', horizontalalignment='right',
+             fontsize=25)
+
+    for i in range(len(ville)):
+
+        y_predict = np.zeros(len(ville))
+        y_predict[i] = 1
+        model_ridge = Ridge(np.exp(logalpha))
+        model_ridge.fit(X, valeur_o3)
+        ridge = model_ridge.predict([y_predict])
+
+        plt.scatter(i, np.mean(valeur_o3[int(np.sum(len_villes[0:i])):
+                    int(np.sum(len_villes[0:i+1]))]), marker='_', lw=10,
+                    color=current_palette[i], s=600)
+
+        plt.text(i + 0.2, np.mean(valeur_o3[int(np.sum(len_villes[0:i])):
+                 int(np.sum(len_villes[0:i+1]))]), '$\\bar y _{}$'.format(i),
+                 verticalalignment='bottom', horizontalalignment='left',
+                 color=current_palette[i], fontsize=20)
+
+        plt.scatter(i, ridge, s=400, marker='_',
+                    color=current_palette[len(ville)+i], lw=5,
+                    label='ridge_{}'.format(ville[i]))
+
+    plt.legend(loc='upper left', bbox_to_anchor=(1., .9))
+    plt.tight_layout()
+    plt.title('Evolution de l estimateur Ridge suivant la valeur \n'
+              'du critère de pénalisation alpha')
+
+
+import ipywidgets as ip
+ip.interact(iteration_ridge_alpha, logalpha=(-10, 10, 0.1))
+
+## Nous avons fais une régression médiane sur variables catégorielles
+## La Régression Ridge est quant à elle sur variables continues
+
+# On remarque que les estimations de Rigde se trouvent toutes autour de la moyenne.
+# Pour Lambda égale à zéro l'estimateur OLS donne la moyenne des observation. Notre régression médiane 
+# donne la médiane des observations.
+# Cela explique des résultats presque similaires.
